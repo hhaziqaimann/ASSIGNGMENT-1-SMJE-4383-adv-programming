d6d9fd61b6f18cf2c56fb9ea62dfc078e0cdd206
hhaziqaimann <116102442+hhaziqaimann@users.noreply.github.com>
2023-01-17 15:32:48 +0800

    Add files via upload

---
+++ sendmail.py
@@ -0,0 +1,48 @@
+import base64
+import smtplib
+import ssl
+from email.mime.text import MIMEText
+from email.utils import formatdate
+
+def sendmail():
+    main_text = "BABI "
+    charset = "utf-8"
+    if charset == "utf-8":
+        msg = MIMEText(main_text, "plain", charset)
+    elif charset == "iso-2022-jp":
+        msg = MIMEText(base64.b64encode(main_text.encode(charset, "ignore")), "plain", charset)
+
+
+    msg.replace_header("Content-Transfer-Encoding", "base64")
+    msg["Subject"] = "!!BABI ANAK ANJING!!"
+    msg["From"] = "aimanhaziq0978@gmail.com"
+    msg["To"] = "danishdnial1998@gmail.com"
+    msg["Date"] = formatdate(None,True)
+
+
+    host = "smtp.gmail.com"
+    nego_combo = ("ssl", 465) 
+
+    if nego_combo[0] == "no-encrypt":
+        smtpclient = smtplib.SMTP(host, nego_combo[1], timeout=10)
+    elif nego_combo[0] == "starttls":
+        smtpclient = smtplib.SMTP(host, nego_combo[1], timeout=10)
+        smtpclient.ehlo()
+        smtpclient.starttls()
+        smtpclient.ehlo()
+    elif nego_combo[0] == "ssl":
+        context = ssl.create_default_context()
+        smtpclient = smtplib.SMTP_SSL(host, nego_combo[1], timeout=10, context=context)
+    smtpclient.set_debuglevel(2) 
+
+
+    username = "aimanhaziq0978@gmail.com"
+    password = "uiegevqstlnqwadm"
+    smtpclient.login(username, password)
+
+
+    smtpclient.send_message(msg)
+    smtpclient.quit()
+
+if __name__ == "__main__":
+    sendmail()
